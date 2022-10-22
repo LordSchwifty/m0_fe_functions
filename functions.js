e06aec95511bf72e827c792b35866053bdd65fdd
Tom Doder <tdoder89@gmail.com>
2022-10-22 13:21:54 -0500

    give greeting edits

--- functions.js
+++ functions.js
@@ -2,7 +2,9 @@
 function printGreeting(first, last) {
     console.log(`Hi ${first} ${last} welcome to summercamp!`);
 }
-     printGreeting("Tom", "Doder")
+     printGreeting("Tom", "Doder");
+     printGreeting("Susan","Boston");
+     printGreeting("Steve", "Jobs");
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 function friends(first, second, third) {
     console.log(`My best friends are ${first}, ${second}, and ${third}`);
@@ -13,9 +15,9 @@ function friends(first, second, third) {
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 function payRange(name, min, max) {
     var range = max - min;
-    console.log(`${name} has a pay range of ${range}`)
+    console.log(`${name} has a pay range of ${range}`);
 }
-    payRange("Amazon", 45000, 250000)
+    payRange("Amazon", 45000, 250000);
 // 4: Write a function that satifies the following interaction pattern:
 
 checkStock(4, "Coffee");
