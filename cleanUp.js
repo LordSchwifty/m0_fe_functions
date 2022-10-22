b968919723b6a097b7896d6a2f6342a7bc38b52a
Tom Doder <tdoder89@gmail.com>
2022-10-21 19:35:06 -0500

    rewrite average function

--- cleanUp.js
+++ cleanUp.js
@@ -30,22 +30,32 @@ function addThreeNums(num1, num2, num3) {
 // "console.log" statement
 
 // EX 3:
-func makeFreshPesto()
-  
-    var avg = sum / 2; {
-
-  console.log(`the average is: ${avg}`.)
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+  
   console.log("Pulse basil and pine nuts");
+  
   console.log("Add garlic and cheeses");
+  
   console.log("Slowly pour in oil");
-  console.log("Season");    
+  
+  console.log("Season");
 }
 
 makeFreshPesto();
-
+// Part of the function was meant for the next example so I removed it. I also
+// noticed that the syntax for "function" was incorrect so I changed it. I also
+// reformated the function.
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
+
+
+    function average(num1, num2) {
+      var sum = num1 + num2
+      var avg = sum / 2
+          console.log(`the average is: ${avg}`)
+        }
+       
+     average(2,3);
+     //Here i reordered the code. Put the curly brackets in the correct places. Took the period off
+//the end of the "console.log statement" and rewrote the last statement.
