5b2fc9d3fcd1a37f0a96ed7a509fe140752a4534
Tom Doder <tdoder89@gmail.com>
2022-10-28 15:35:19 -0500

    give corrections

--- functions.js
+++ functions.js
@@ -1,23 +1,23 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 function printGreeting(first, last) {
-    console.log(`Hi ${first} ${last} welcome to summercamp!`);
+ console.log(`Hi ${first} ${last} welcome to summercamp!`);
 }
-     printGreeting("Tom", "Doder");
-     printGreeting("Susan","Boston");
-     printGreeting("Steve", "Jobs");
+ printGreeting("Tom", "Doder");
+ printGreeting("Susan","Boston");
+ printGreeting("Steve", "Jobs");
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 function friends(first, second, third) {
-    console.log(`My best friends are ${first}, ${second}, and ${third}`);
-  }
-  
-  friends("Jack", "Max", "Will");
-
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-function payRange(name, min, max) {
-    var range = max - min;
-    console.log(`${name} has a pay range of ${range}`);
+ console.log(`My best friends are ${first}, ${second}, and ${third}`);
 }
-    payRange("Amazon", 45000, 250000);
+  
+ friends("Jack", "Max", "Will");
+
+// 3: e a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+function payRange(name, min, max) {
+ var range = max - min;
+ console.log(`${name} has a pay range of ${range}`);
+}
+ payRange("Amazon", 45000, 250000);
 // 4: Write a function that satifies the following interaction pattern:
 
 checkStock(4, "Coffee");
@@ -33,10 +33,12 @@ checkStock(1, "Salsa");
 // => "Salsa - running LOW"
 
 
-function kitchenStock(Coffee, Tortillas, Cheese, Salsa) {
-    console.log(` ${Coffee} is stocked`);
-    console.log(` ${Tortillas} - running LOW`);
-    console.log(` ${Cheese} - OUT of stock!`);
-    console.log(` ${Salsa} - running LOW`);
+function checkStock(quantity, item){
+if (quantity > 3){
+ console.log(`${item} is stocked`);
+} else if (quantity > 0){
+ console.log(`${item} - running LOW`);
+} else {
+ console.log(`${item} - OUT of stock`);
 }
-    kitchenStock("Coffee", "Tortillas", "Cheese", "Salsa");
+}
